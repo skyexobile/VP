efeb2cdbd3af66390703b7c61f983f5c093f0698
Angela Chan <Skye@main-10-230-126-137.tamulink.tamu.edu>
2019-05-13 15:09:27 -0500

    t s quit uit() quit() exit()

---
+++ vpClient.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+import sys, csv
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 1234
+
+
+uname = str(sys.argv[2])
+
+ip =uname = str(sys.argv[1])
+s.connect((ip, port))
+s.send(uname.encode('ascii'))
+def csv_writer(data, path):
+    with open(path, "a", newline = '') as csv_file:
+        #writer = csv.writer(csv_file, delimiter = ' ')
+        csv_file.write(str(data) + '\n')
+        csv_file.close()
+    print('done')
+clientRunning = True
+#path = "DataFiles/" + str(subID)+ "/touch_data.csv"
+#f = open(path,'a')
+def receiveMsg(sock):
+    serverDown = False
+    while clientRunning and (not serverDown):
+        try:
+            msg = sock.recv(1024).decode('ascii')
+            print('messaged receive is ' +msg)
+            #csv_writer(msg, path)
+        except:
+            print('Server is Down. You are now Disconnected. Press enter to exit...')
+            serverDown = True
+
+threading.Thread(target = receiveMsg, args = (s,)).start()
+while clientRunning:
+    tempMsg = input()
+    msg = uname + '>>' + tempMsg
+    if '**quit' in msg:
+        clientRunning = False
+        s.send('**quit'.encode('ascii'))
+    else:
+        s.send(msg.encode('ascii'))
---
+++ vpServer.py
@@ -0,0 +1,79 @@
+import socket
+import threading
+import time, sys
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverRunning = True
+ip = str(socket.gethostbyname(socket.gethostname()))
+port = 1234
+
+clients = {}
+
+s.bind((ip, port))
+s.listen()
+print('Server Ready...')
+print('Ip Address of the Server::%s'%ip)
+def csv_writer(data, path):
+    with open(path, "a", newline = '') as csv_file:
+        #writer = csv.writer(csv_file, delimiter = ' ')
+        csv_file.write(str(data) + '\n')
+        csv_file.close()
+    print('done')
+#path = "DataFiles/" + str(subID)+ "/psycho_data.csv"
+def handleClient(client, uname):
+    clientConnected = True
+    keys = clients.keys()
+
+    while clientConnected:
+        try:
+            msg = client.recv(1024).decode('ascii')
+            response = 'Number of People Online\n'
+            found = False
+            print('this is the message im getting' + msg)
+            if '**broadcast' in msg:
+                msg = msg.replace('**broadcast','')
+                for k,v in clients.items():
+                    v.send(msg.encode('ascii'))
+            elif '**quit' in msg:
+                response = 'Stopping Session and exiting...'
+                client.send(response.encode('ascii'))
+                for i in clients:
+                    clients.pop(uname)
+                    print(uname + ' has been logged out')
+                clientConnected = False
+            else:
+                for name in keys:
+                    if('**'+name) in msg:
+                        ticks = time.time()
+                        if name != 'touch':
+                            msg = msg.replace('**'+name, ','+str(ticks) )
+                        else:
+                            temp_msg = msg.replace('**'+name, ',' +str(ticks) )
+                            msg = msg.replace('**'+name, '')
+                            print(msg)
+                            #csv_writer(temp_msg, path)
+                        print('this name is ', name)
+                        clients.get(name).send(msg.encode('ascii'))
+                        found = True
+                if(not found):
+                    client.send('Trying to send message to invalid person.'.encode('ascii'))
+        except:
+            clients.pop(uname)
+            print(uname + ' has been logged out')
+            clientConnected = False
+
+
+
+
+
+
+while serverRunning:
+    client, address = s.accept()
+    uname = client.recv(1024).decode('ascii')
+    print('%s connected to the server'%str(uname))
+    client.send('Welcome to Messenger.'.encode('ascii'))
+
+    if(client not in clients):
+        clients[uname] = client
+        threading.Thread(target = handleClient, args = (client, uname, )).start()
