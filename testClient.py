3eb6d0f5c8f2feb7e120f8f0d0dea88a55ee736e
Angela Chan <Skye@Angelas-MacBook-Air-1890.local>
2019-07-06 21:34:10 -0500

    add

--- testClient.py
+++ testClient.py
@@ -1,22 +1,27 @@
-import serial, datetime, time, re, pickle, os, select, sys, csv, threading
-import numpy as np
+
+import tkinter as tkin
+
+import serial, datetime, time, re
+
+import os, select, sys, csv, threading
 from time import gmtime, strftime
 import socket
-import tkinter as tk
-from tkinter import*
+
+
 import csv
+from subprocess import Popen, PIPE
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 8000
 
-uname = str(sys.argv[2])
+uname = 'PartnerA'
 
 ip = str(sys.argv[1])
 s.connect((ip, port))
 s.send(uname.encode('ascii'))
 print("Connecting to Touch Input")
-input_serial = serial.Serial('/dev/cu.usbmodem14101')
+input_serial = serial.Serial('/dev/cu.usbmodem14201')
 input_serial.baudrate =115200
 input_serial.setDTR(False)
 input_serial.setRTS(False)
@@ -33,37 +38,38 @@ print("Connected!")
 soft_value = 100
 medium_value = 500
 hard_value = 1000
-root = tk.Tk()
-acquired_flag = False
-previous_read = -100
+root = tkin.Tk()
+soft_flag = False
+medium_flag = False
+hard_flag = False
+released_flag = False
+previous_value = 0
 offset = 0
-
-
-
-
-
-
-
-
-'''
-
-print("Connecting to Touch Output")
-output_serial = serial.Serial('/dev/cu.usbmodem1411')
-output_serial.setBaudrate(115200)
-
-output_serial.setDTR(False)
-output_serial.setRTS(False)
-
-'''
-
+touch_record = []
+raw_send =[]
+raw_receive = []
+playing = False
+sent_list=[]
+final_count=0
+recv_record = []
+recording_time = time.time()
+sent_dict = {}
+sent_count=1
+replay_count = 1
+touch_tuple =[]
+sent_record = []
+videoRecording = False
+sent_list=[]
+recv_list= []
 def record():
-    scpt ='''
+    global recording_time
+    global videoRecording
+    scpt = '''
     tell application "System Events"
     	keystroke "%" using command down
     	delay 1.0
     	keystroke return
-    end tell
-    '''
+    end tell'''
 
 
     args = ['2', '2']
@@ -71,8 +77,12 @@ def record():
 
     p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     stdout, stderr = p.communicate(scpt)
-    recording_time = time.time()
-    s.send('**time'.encode('ascii'))
+    recording_time = float(time.time())
+    #s.send('**time'.encode('ascii'))
+    videoRecording = True
+    print('recording time is ', recording_time)
+
+
 def reset():
     global offset
     input_serial.write(str("0").encode())
@@ -138,7 +148,7 @@ def set_medium():
     global offset
     value_list = []
     max_list = []
-    global med_value
+    global medium_value
     counter = 0
     previous = -100
     while counter <3:
@@ -183,7 +193,7 @@ def set_medium():
                     input_value = float(value) + offset
         elif input_value < -3:
             offset = offset - input_value
-    med_value = sum(max_list)/len(max_list)
+    medium_value = sum(max_list)/len(max_list)
     print("Medium touches have been trained")
 def set_hard():
     global offset, hard_value
@@ -237,39 +247,45 @@ def set_hard():
     hard_value = sum(max_list)/len(max_list)
     print("Hard touches have been trained")
 def save_settings():
-    global soft_value, med_value, hard_value, PID_value, medium_value
-    if soft_value > med_value:
-        temp = soft_value
-        soft_value = med_value
-        med_value = temp
-    if med_value > hard_value:
-        temp = med_value
-        med_value = hard_value
-        hard_value = temp
-    medium_value = med_value
+    global soft_value, hard_value, PID_value, medium_value
+
     print("Your settings have been saved!")
     print("soft is ", soft_value)
     print("medium is ", medium_value)
     print("hard is ", hard_value)
 
-
-reset_button =  tk.Button(root, text = "Reset Sensor", command = reset)
-soft_button =  tk.Button(root, text = "Define Soft", command = set_soft)
-medium_button =  tk.Button(root, text = "Define Medium", command = set_medium)
-hard_button  =  tk.Button(root, text = "Define Hard", command = set_hard)
-save_button  =  tk.Button(root, text = "Save Settings", command = save_settings)
-
-reset_button.pack(side = tk.BOTTOM)
-soft_button.pack(side = tk.BOTTOM)
-medium_button.pack(side = tk.BOTTOM)
-hard_button.pack(side = tk.BOTTOM)
-save_button.pack(side = tk.BOTTOM)
+def save_sent():
+    global sent_list
+    csv_writer(sent_list, 'sent_data.csv')
+    print('done')
+def save_recv():
+    global recv_list
+    csv_writer(recv_list, 'received_data.csv')
+    print('done')
+reset_button =  tkin.Button(root, text = "Reset Sensor", command = reset)
+soft_button =  tkin.Button(root, text = "Define Soft", command = set_soft)
+medium_button =  tkin.Button(root, text = "Define Medium", command = set_medium)
+hard_button  =  tkin.Button(root, text = "Define Hard", command = set_hard)
+save_button  =  tkin.Button(root, text = "Save Settings", command = save_settings)
+#replay_instance = tkin.Button(root, text = "Instance Replay", command = replay)
+record_video = tkin.Button(root, text = "Record", command = record)
+save_sTouch = tkin.Button(root, text = 'Save Sent', command = save_sent)
+save_rTouch = tkin.Button(root, text = 'Save Received', command = save_recv)
+save_rTouch.pack()
+record_video.pack()
+#replay_instance.pack()
+save_sTouch.pack()
+reset_button.pack(side = tkin.BOTTOM)
+soft_button.pack(side = tkin.BOTTOM)
+medium_button.pack(side = tkin.BOTTOM)
+hard_button.pack(side = tkin.BOTTOM)
+save_button.pack(side = tkin.BOTTOM)
 
 #root.update()
 def csv_writer(data, path):
-    with open(path, "a", newline = '') as csv_file:
-        #writer = csv.writer(csv_file, delimiter = ' ')
-        csv_file.write(str(data) + '\n')
+    with open(path, "a", newline = '\n') as csv_file:
+        writer = csv.writer(csv_file, delimiter = '\n')
+        writer.writerow(data)
         csv_file.close()
     print('done')
 clientRunning = True
@@ -277,6 +293,7 @@ clientRunning = True
 #f = open(path,'a')
 
 def receiveMsg(sock):
+    global recording_time, recv_list
     serverDown = False
     while clientRunning and (not serverDown):
 
@@ -285,59 +302,153 @@ def receiveMsg(sock):
             if '**send' in msg:
                 msg = msg[(msg.find('**send')+6):]
                 received.append(msg)
-                print('this is the list: ',received)
+                #print('this is the list: ',received)
             elif '**vtime' in msg:
                 msg = msg[msg.find('**vtime')+8:]
                 recording_time = float(msg) + 4
-                print('video recording time is ', recording_time)
+                #print('video recording time is ', recording_time)
             #csv_writer(msg, path)
             elif 'soft' in msg:
-                print("soft squeeze")
+                #print("soft squeeze REceived")
                 #output_serial.write(str('B').encode())
                 #output_serial.readline().decode()
+                print('message received is ' +msg)
+                tempRecord =  float(time.time())-(recording_time)
+                recv_list.append(str(tempRecord) + ',soft')
+                #print("touch record is ", touch_record)
+                touch_record.append(msg)
             elif 'medium' in msg:
-                print("medium squeeze")
+                #print("medium squeeze Received")
                 #output_serial.write(str('E').encode())
                 #output_serial.readline().decode()
+                print('message received is ' +msg)
+                tempRecord =  float(time.time())-(recording_time)
+                recv_list.append(str(tempRecord)+ ',medium')
+                #print("touch record is ", touch_record)
+                touch_record.append(msg)
             elif 'hard' in msg:
-                print("hardsqueeze")
+                #print("hardsqueeze received")
                 #output_serial.write(str('H').encode())
                 #output_serial.readline().decode()
-            #print('message received is ' +msg)
+                print('message received is ' +msg)
+                tempRecord =  float(time.time())-(recording_time)
+                recv_list.append(str(tempRecord)+ ',hard')
+                touch_record.append(msg)
+                #print("touch record is ", touch_record)
+            elif 'release' in msg:
+                #print("release received")
+                #output_serial.write(str('H').encode())
+                #output_serial.readline().decode()
+                print('message received is ' +msg)
+                tempRecord =  float(time.time())-(recording_time)
+                recv_list.append(str(tempRecord)+ ',release')
+                touch_record.append(msg)
+                #print("touch record is ", touch_record)
 
         except:
             print('Server is Down. You are now Disconnected. Press enter to exit...')
             serverDown = True
 
 threading.Thread(target = receiveMsg, args = (s,)).start()
-
+tempMsg = ''
 while clientRunning:
-    #root.update()
-    # a.encode('utf-8').strip()
-    #print('A')
-    #print('input value is ', input_value)
-    print('first test')
+    root.update()
+    #print('video recording is ', videoRecording)
+    while videoRecording is True:
+        root.update()
+        value = (input_serial.readline().decode())
+        input_value = float(value)
+        if  abs(input_value) - abs(previous_value) >0:
+            if input_value >= soft_value and input_value < medium_value:
+                if soft_flag is False:
+                    tempMsg = 'soft'
+                    tempRecord = float(time.time())-(recording_time)
+                    sent_list.append(str(tempRecord)+ ','+ tempMsg)
+                    sent_count+=1
+                    released_flag=False
+                else:
+                    tempMsg = ''
+                soft_flag = True
+                medium_flag = False
+                hard_flag = False
+            elif input_value >= medium_value and input_value < hard_value:
+                if medium_flag is False:
+                    tempMsg = 'medium'
+                    tempRecord =  float(time.time())-(recording_time)
+                    sent_list.append(str(tempRecord)+ ','+ tempMsg)
+                    sent_count+=1
+                    released_flag=False
 
-    value = (input_serial.readline().decode())
-    input_value = float(value)
-    print(input_value)
-    print('testB')
-    if input_value >= soft_value and input_value < medium_value:
-        tempMsg = 'soft'
-    elif input_value >= medium_value and input_value < hard_value:
-        tempMsg = 'medium'
-    elif input_value >= hard_value:
-        tempMsg = 'hard'
-    else:
-        tempMsg = ''
+                else: tempMsg = ''
+                medium_flag = True
+                soft_flag = False
+                hard_flag = False
+            elif input_value >= hard_value:
+                if hard_flag is False:
+                    tempMsg = 'hard'
+                    tempRecord =  float(time.time())-(recording_time)
+                    sent_list.append(str(tempRecord)+ ','+ tempMsg)
 
-    msg = '**' +uname + '>>' + tempMsg
-    if '**quit' in msg:
-        clientRunning = False
-        s.send('**quit'.encode('ascii'))
-    else:
-        if '**vtime' in msg:
-            record()
-        elif '**rtime' in msg:
-            print('recording time is ', datetime.datetime.now())
-        s.send(msg.encode('ascii'))
+                    sent_count+=1
+                    released_flag=False
+
+                else:
+                    tempMsg = ''
+                hard_flag = True
+                soft_flag = False
+                medium_flag = False
+            else:
+                tempMsg = ''
+
+        else:
+            #check for if released
+            if released_flag is False:
+                if soft_flag is True:
+                    if input_value < soft_value:
+                        tempMsg = 'release'
+                        tempRecord =  float(time.time())-(recording_time)
+                        sent_list.append(str(tempRecord)+ ','+ tempMsg)
+
+                        sent_count+=1
+                        soft_flag=False
+                        released_flag=True
+                    else:
+
+                        tempMsg = ''
+                if medium_flag is True:
+                    if input_value < medium_value:
+                        tempMsg = 'release'
+                        tempRecord = float(time.time())-(recording_time)
+                        sent_list.append(str(tempRecord)+ ','+ tempMsg)
+
+                        sent_count+=1
+                        medium_flag=False
+                        released_flag=True
+                    else:
+                        tempMsg = ''
+                if hard_flag is True:
+                    if input_value < hard_value:
+                        hard_flag = False
+                        tempMsg = 'release'
+                        tempRecord = float(time.time())-(recording_time)
+                        sent_list.append(str(tempRecord)+ ','+ tempMsg)
+                        sent_count+=1
+                        hard_flag=False
+                        released_flag=True
+                    else:
+                        tempMsg = ''
+
+
+        #print(input_value)
+        msg = '**' +'PartnerA' + '>>' + tempMsg
+        if '**quit' in msg:
+            clientRunning = False
+            s.send('**quit'.encode('ascii'))
+        else:
+            if '**vtime' in msg:
+                record()
+            elif '**rtime' in msg:
+                print('recording time is ', datetime.datetime.now())
+            s.send(msg.encode('ascii'))
+            tempMsg = ''
+        previous_value = input_value
--- video.py
+++ video.py
@@ -1,7 +1,18 @@
-# importing libraries
-from moviepy.editor import*
+
+
+import os
+from tkinter import Tk
 #clip = VideoFileClip('output.mp4')
-clip = VideoFileClip('out.mov')
-clip = clip.resize(0.3)
-clip = clip.subclip(2, 4)
-clip.preview(fps=50)
+
+root =Tk()
+from moviepy.editor import *
+
+for f_name in os.listdir('../VP'):
+    if f_name.endswith('.mov'):
+        print(f_name)
+        clip = VideoFileClip(f_name)
+
+        clip = VideoFileClip(f_name)
+        clip = clip.resize(0.5)
+        clip = clip.subclip(2, 8)
+        clip.preview()
